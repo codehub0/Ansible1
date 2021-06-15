52afa01ea23ddb2e8ca38ca0545075a33e56d2d0
adsingh1 <adsingh327@yahoo.com>
2021-06-15 13:38:41 -0500

    update codes

--- instance-launch/launch.sh
+++ instance-launch/launch.sh
@@ -7,19 +7,20 @@ if [ -z "${COMPONENT}" ]; then
   echo "Component Input is Needed"
   exit 1
 fi
-LID=lt-0fb7a0a2ebe886b59
-LVER=1
+
+LID=lt-090f6832097248547
+LVER=2
 
 ## Validate If Instance is already there
 
 DNS_UPDATE() {
-  PRIVATEIP=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1)
+  PRIVATEIP=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1 | grep -v null)
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z01439762OTRH1XUKPA3P --change-batch file:///tmp/record.json | jq
+  aws route53 change-resource-record-sets --hosted-zone-id Z04221902CM9ZT2GHM1NW --change-batch file:///tmp/record.json | jq
 }
 
 INSTANCE_CREATE() {
-  INSTANCE_STATE=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].State.Name | xargs -n1)
+  INSTANCE_STATE=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].State.Name | xargs -n1 | grep -v terminated)
   if [ "${INSTANCE_STATE}" = "running" ]; then
     echo "${COMPONENT} Instance already exists!!"
     DNS_UPDATE
@@ -32,7 +33,7 @@ INSTANCE_CREATE() {
   fi
 
   echo -n Instance ${COMPONENT} created - IPADDRESS is
-  aws --region us-east-1 ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq | grep  PrivateIpAddress  |xargs -n1
+  aws --region us-east-1 ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq .Reservations[].Instances[].PrivateIpAddress  |xargs -n1
   sleep 10
   DNS_UPDATE
 }
